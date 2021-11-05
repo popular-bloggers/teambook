9d2a25a7bb46e65efb328f4565aec1c5575b42c7
Daniil <dshindov@protonmail.com>
2021-11-05 13:39:14 +0700

    Add Fenwick Tree implementation

---
+++ content/Структуры данных/Дерево Фенвика.hpp
@@ -0,0 +1,31 @@
+//
+//  Дерево Фенвика
+//  Позволяет выполнять запросы на префиксе массива.
+//  В дереве используется 1-индексация.
+//
+
+struct FenwickTree {
+  static int F(int x) { return x & -x; }
+  int n;
+  vector<int> t;
+  FenwickTree(int n) : n(n), t(n + 1, 0) {}
+  void update(int i, int d) {  // a[i] += d, i \in [1; n]
+    for (; i <= n; i += F(i)) t[i] += d;
+  }
+  int get(int r) {  // сумма на отрезке [1; r]
+    int res = 0;
+    for (; r > 0; i -= F(i)) res += t[i];
+    return res;
+  }
+  int lower_bound(int sum) {  // вернёт первое r такое, что get(r) >= sum
+    // или n + 1, если такого r нет
+    const int K = 20;
+    int i = 0;
+    for (int k = 1 << K; k > 0; k >>= 1) {
+      if (i + k <= n && t[i + k] < sum) {
+        sum -= t[i += k];
+      }
+    }
+    return i + 1;
+  }
+};
