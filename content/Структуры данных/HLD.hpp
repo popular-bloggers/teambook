56d099099062d26adf5d2eb834635d839fc0b491
Daniil <dshindov@protonmail.com>
2021-11-05 13:40:47 +0700

    Typo fixes, add some comments

--- content/Структуры данных/HLD.hpp
+++ content/Структуры данных/HLD.hpp
@@ -1,25 +1,20 @@
 //
 //  Heavy-Light Decomposition
-//  Позволяет выполнять запросы на путях в дереве
+//  Позволяет выполнять запросы на путях в дереве.
 //  Источник: https://codeforces.com/blog/entry/53170
 //
-
-#include <Дерево отрезков.hpp>
 
 struct HLD {
   int n, t = 0;
   vector<vector<int>> g;
   vector<int> tin, siz, par, h, up;
-
   SegmentTree st;
-
   HLD(int n, const vector<vector<int>> &g)
       : n(n), g(g), tin(n), siz(n, 1), par(n), h(n), up(n), st(n) {
     par[0] = -1, h[0] = 0, up[0] = 0;
     dfs_sz(0);
     dfs_up(0);
   }
-
   void dfs_sz(int v) {
     if (par[v] != -1) g[v].erase(find(all(g[v]), p));
     for (int &u : g[v]) {
@@ -29,7 +24,6 @@ struct HLD {
       if (siz[u] > siz[g[v][0]]) swap(u, g[v][0]);
     }
   }
-
   void dfs_up(int v) {
     tin[v] = t++;
     for (int u : g[v]) {
@@ -40,17 +34,16 @@ struct HLD {
       dfs_up(u);
     }
   }
-
-  int get_path(int v, int u) {
+  int get_path(int v, int u) {  // запрос на пути от v до u
     int res = 0;
     for (; up[v] != up[u]; v = par[up[v]]) {
       if (h[up[v]] < h[up[u]]) swap(v, u);
       res += st.get(tin[up[v]], tin[v] + 1);
+      // если значения на рёбрах, прибавить к левой границе +1
     }
     if (h[v] > h[u]) swap(v, u);
     res += st.get(tin[v], tin[u] + 1);
     return res;
   }
-
   int get_subtree(int v) { return st.get(tin[v], tin[v] + siz[v]); }
-}
+};
